5b1f4b9a05e0f44fe41d5ba84c93959271aac0b6
Karolis_portatil <karolisjakas04@gmail.com>
2024-04-02 09:45:09 +0200

    First commit

---
+++ src/main/java/com/karolis/jdbc_sql_kj/HelloController.java
@@ -0,0 +1,14 @@
+package com.karolis.jdbc_sql_kj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.karolis.jdbc_sql_kj {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.karolis.jdbc_sql_kj to javafx.fxml;
+    exports com.karolis.jdbc_sql_kj;
+}
